766c261ab15e54e347eb2788c625cd6ed80f5ef8
Rafael Costa <rafaelcost@gmail.com>
2026-05-13 13:29:46 -0300

    Initial commit

---
+++ app/src/main/java/com/rafaelcosta/carteirinhadigital2devest_b/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.rafaelcosta.carteirinhadigital2devest_b
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rafaelcosta.carteirinhadigital2devest_b.ui.theme.CarteirinhaDigital2DEVEST_BTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CarteirinhaDigital2DEVEST_BTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CarteirinhaDigital2DEVEST_BTheme {
+        Greeting("Android")
+    }
+}
