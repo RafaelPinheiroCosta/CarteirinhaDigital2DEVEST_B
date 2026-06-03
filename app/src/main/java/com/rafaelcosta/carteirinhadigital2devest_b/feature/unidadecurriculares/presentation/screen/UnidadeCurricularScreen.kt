127f5cd51477ae11e2d44a92ca1272574489a431
Rafael Costa <rafaelcost@gmail.com>
2026-06-03 16:07:45 -0300

    atualização 03/06/2026

--- app/src/main/java/com/rafaelcosta/carteirinhadigital2devest_b/feature/unidadecurriculares/presentation/screen/UnidadeCurricularScreen.kt
+++ app/src/main/java/com/rafaelcosta/carteirinhadigital2devest_b/feature/unidadecurriculares/presentation/screen/UnidadeCurricularScreen.kt
@@ -7,23 +7,37 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.rafaelcosta.carteirinhadigital2devest_b.feature.unidadecurriculares.data.dataSource
 import com.rafaelcosta.carteirinhadigital2devest_b.feature.unidadecurriculares.domain.model.UnidadeCurricular
 import com.rafaelcosta.carteirinhadigital2devest_b.feature.unidadecurriculares.presentation.component.UnidadeCurricularCard
 
 @Composable
 fun UnidadeCurricularScreen(
-    modifier: Modifier = Modifier,
-    unidadesCurriculares : List<UnidadeCurricular>
+    modifier: Modifier = Modifier
 ) {
+    val unidadesCurriculares = dataSource()
+
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 16.dp),
+            .padding(
+                horizontal = 16.dp,
+                vertical = 12.dp
+            ),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         items(unidadesCurriculares) { unidadeCurricular ->
             UnidadeCurricularCard(unidadeCurricular = unidadeCurricular)
         }
     }
+}
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun UnidadeCurricularScreenPreview() {
+    UnidadeCurricularScreen()
 }
