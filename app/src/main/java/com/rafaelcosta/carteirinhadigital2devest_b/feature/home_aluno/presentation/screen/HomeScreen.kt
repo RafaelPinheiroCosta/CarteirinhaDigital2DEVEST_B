10bd4c0ae21569e2f84c8512c2631c1022ea10e4
Rafael Costa <rafaelcost@gmail.com>
2026-08-14 14:55:45 -0300

    14/08/2026

--- app/src/main/java/com/rafaelcosta/carteirinhadigital2devest_b/feature/home_aluno/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/rafaelcosta/carteirinhadigital2devest_b/feature/home_aluno/presentation/screen/HomeScreen.kt
@@ -1,2 +1,69 @@
 package com.rafaelcosta.carteirinhadigital2devest_b.feature.home_aluno.presentation.screen
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier= Modifier
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Aluno",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {}
+            ) {
+                Text(
+                    text = "Carteirinha"
+                )
+            }
+            Button(
+                onClick = {}
+            ) {
+                Text(
+                    text = "Unidades Curriculares"
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize()
+    )
+}
