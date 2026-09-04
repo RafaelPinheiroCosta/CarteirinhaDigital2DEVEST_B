f088a28d0935a19416a144fb775c4ca09c1d4cb9
Rafael Costa <rafaelcost@gmail.com>
2026-09-04 15:38:40 -0300

    04/09/2026

---
+++ app/src/main/java/com/rafaelcosta/carteirinhadigital2devest_b/feature/login/data/remote/dto/ErrorResponseDTO.kt
@@ -0,0 +1,4 @@
+package com.rafaelcosta.carteirinhadigital2devest_b.feature.login.data.remote.dto
+
+class ErrorResponseDTO {
+}
---
+++ app/src/main/java/com/rafaelcosta/carteirinhadigital2devest_b/feature/login/data/remote/network/NetworkFactory.kt
@@ -0,0 +1,6 @@
+package com.rafaelcosta.carteirinhadigital2devest_b.feature.login.data.remote.network
+
+object NetworkFactory {
+    private const val BASE_URL ="http://10.0.2.2:8080/"
+
+}
